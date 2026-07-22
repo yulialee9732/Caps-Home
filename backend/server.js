ad8912c506fc8e87cab2b0b909acb08fe9344c94
Seungyoun Lee <yulialee217@gmail.com>
2026-07-22 01:18:47 -0400

    fix Gmail SMTP port

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,9 @@ async function getNumericSheetId() {
 
 // ─── Nodemailer ───────────────────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
